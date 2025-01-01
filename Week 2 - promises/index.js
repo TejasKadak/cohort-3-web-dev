1bc4e9c04dce2fb463ba27973b35bbc5cb6ca58e
tejaskadak <tejas.02001@gmail.com>
2025-01-01 20:03:32 +0530

    Update index.js

--- Week 2 - promises/index.js
+++ Week 2 - promises/index.js
@@ -1,23 +1,27 @@
-class Rectangle{
-    constructor(height, width, color){
-        this.height = height;
-        this.width = width;
-        this.color = color;
-    }
+// class Rectangle{
+//     constructor(height, width, color){
+//         this.height = height;
+//         this.width = width;
+//         this.color = color;
+//     }
 
-    area(){
-        const area = this.height * this.width;
-        return area;
-    }
+//     area(){
+//         const area = this.height * this.width;
+//         return area;
+//     }
 
-    print(){
-        console.log("painting with color " + this.color);
-        console.log(this);
-    }
-}
+//     print(){
+//         console.log("painting with color " + this.color);
+//         console.log(this);
+//     }
+// }
 
 
-const rect = new Rectangle(2, 4,"red");
-const area = rect.area();
-console.log(area);
-const print = rect.print();
+// const rect = new Rectangle(2, 4,"red");
+// const area = rect.area();
+// console.log(area);
+// const print = rect.print();
+
+
+const now = new Date();
+console.log(now.getDay());
