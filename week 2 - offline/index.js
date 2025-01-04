ddeebb90f6724453b16698b1726b0f812d1e1572
tejaskadak <tejas.02001@gmail.com>
2025-01-04 18:46:02 +0530

    Update index.js

--- week 2 - offline/index.js
+++ week 2 - offline/index.js
@@ -3,3 +3,19 @@ function setTimeoutPromisified(duration){
         setTimeout(resolve, duration);
     });
 }
+
+// // promise chaining
+// setTimeoutPromisified(1000).then(function (){
+//     console.log("hi");
+//     return setTimeoutPromisified(3000)
+// }).then(function(){
+//     console.log("hello");
+//     return setTimeoutPromisified(5000)
+// }).then(function (){
+//     console.log("hi there");
+// });
+
+// console.log("outside the callback hell");
+
+
+// async await
