f156e59ff4214911922694aaeeeda5974f0ea748
tejaskadak <tejas.02001@gmail.com>
2025-02-24 18:56:46 +0530

    updated

--- week-6/Deep-dive-into-http/index.js
+++ week-6/Deep-dive-into-http/index.js
@@ -1,26 +1,28 @@
 const express = require('express');
+const JWT_SECRET = "tejas";
+const jwt = require("jsonwebtoken");
 const app = express();
 app.use(express.json());
 
 const users = [];
 
-function generateToken() {
-    let options = ['a', 'b', 'c', 'd', 'e', 'f',
-         'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
-         'o', 'p', 'q', 'r', 's', 't','u', 'v', 
-         'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 
-         'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-         'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 
-         'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1',
-          '2', '3', '4', '5', '6', '7', '8', '9'];
+// function generateToken() {
+//     let options = ['a', 'b', 'c', 'd', 'e', 'f',
+//          'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+//          'o', 'p', 'q', 'r', 's', 't','u', 'v', 
+//          'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 
+//          'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+//          'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 
+//          'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1',
+//           '2', '3', '4', '5', '6', '7', '8', '9'];
 
-    let token = "";
-    for (let i = 0; i < 32; i++) {
-        // use a simple function here
-        token += options[Math.floor(Math.random() * options.length)];
-    }
-    return token;
-}
+//     let token = "";
+//     for (let i = 0; i < 32; i++) {
+//         // use a simple function here
+//         token += options[Math.floor(Math.random() * options.length)];
+//     }
+//     return token;
+// }
 
 app.post("/signup", (req, res) => {
    const username = req.body.username;
@@ -32,9 +34,9 @@ app.post("/signup", (req, res) => {
    })
 
    res.json({
-    messsage: "you are signed in"
+    messsage: "you have signed up"
    })
-
+console.log(users);
 });
 
 
@@ -45,17 +47,36 @@ app.post("/signin", (req, res) => {
     const user = users.find(user => user.username === username && user.password === password);
 
     if (user) {
-        const token = generateToken();
-        user.token = token;
-        res.send({
-            token
+         const token =  jwt.sign({
+            username: username
+         }, JWT_SECRET);  // convert their username into jwt
+        // user.token = token;
+        res.json({
+            token: token
         })
-        console.log(users);
     } else {
         res.status(403).send({
             message: "Invalid username or password"
         })
     }
+    console.log(users);
 });
 
+
+app.get("/me", (req, res) =>{
+    const token = req.headers.token;
+    const decodedInfo = jwt.verify(token, JWT_SECRET);
+    const username = decodedInfo.username
+
+    const user = users.find(user => user.username === username);
+    if(user){
+        res.send({
+            username: user.username
+        })
+    }else{
+        res.status(401).send({
+            message: "token invalid"
+        })
+    }
+});
 app.listen(3000);
