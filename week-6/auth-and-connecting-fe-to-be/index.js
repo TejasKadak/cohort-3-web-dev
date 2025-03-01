8ee9c29ff76255a3b6f6c1441a5828862bd6a9e5
tejaskadak <tejas.02001@gmail.com>
2025-03-01 18:24:17 +0530

    updated

--- week-6/auth-and-connecting-fe-to-be/index.js
+++ week-6/auth-and-connecting-fe-to-be/index.js
@@ -1,74 +1,77 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const JWT_SECRET = "tejas02";
+
+const JWT_SECRET = "tejas20";
 
 const app = express();
 app.use(express.json());
 
 const users = [];
 
+app.get("/", function(req,res) {
+    res.sendFile(__dirname + "/public/index.html");
+})
 
-app.post("/signup", (req, res) =>{
-  const username = req.body.username;
-  const password = req.body.password;
-  users.push({
-    username: username,
-    password: password
-  })
-
-  res.json({
-    message: "You are signed up"
-  })
-});
-
-app.post("/signin", (req, res) =>{
+// Signup Route - No auth middleware here
+app.post("/signup", function(req, res) {
     const username = req.body.username;
     const password = req.body.password;
 
-    let foundUser = null;
-
-    for(let i=0; i<users.length; i++){
-        if(users[i].username === username && users[i].password === password){
-            foundUser = users[i];
-        }
+    // Check if user already exists
+    const existingUser = users.find(user => user.username === username);
+    if (existingUser) {
+        return res.status(400).json({ message: "User already exists" });
     }
 
-    if(!foundUser){
-        res.json({
-            message: "credentials incorrect"
-        })
-        return;
-    }
-    else{
-        const token = jwt.sign({
-            username
-        },JWT_SECRET);
-        res.json({
-            token: token
-        })
-    }
-    
+    users.push({ username, password });
+
+    res.json({ message: "You are signed up" });
 });
 
-app.get("/get-password", (req, res) =>{
+// Signin Route - No auth middleware here
+app.post("/signin", function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const foundUser = users.find(user => user.username === username && user.password === password);
+
+    if (!foundUser) {
+        return res.status(401).json({ message: "Credentials incorrect" });
+    }
+
+    const token = jwt.sign({ username: foundUser.username }, JWT_SECRET);
+    res.header("jwt", token);
+
+    res.json({ token });
+});
+
+// Authentication Middleware
+function auth(req, res, next) {
     const token = req.headers.token;
-
-    const decodedData = jwt.verify(token, JWT_SECRET);
-
-    if(decodedData.username){
-        let foundUser = null;
-
-        for(let i =0; i<users.length; i++){
-            if(users[i].username === decodedData.username){
-                foundUser = users[i];
-            }
-        }
-
-        res.json({
-            username: foundUser.username,
-            password: foundUser.password
-        })
+    if (!token) {
+        return res.status(401).json({ message: "No token provided" });
     }
+
+    try {
+        const decodedData = jwt.verify(token, JWT_SECRET);
+        req.username = decodedData.username;
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: "Invalid token" });
+    }
+}
+
+// Get Current User Data
+app.get("/me", auth, function(req, res) {
+    const currentUser = req.username;
+
+    const foundUser = users.find(user => user.username === currentUser);
+
+    if (!foundUser) {
+        return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json({ username: foundUser.username, password: foundUser.password });
 });
 
-app.listen(3000);
+app.listen(3000, () => console.log("Server running on port 3000"));
