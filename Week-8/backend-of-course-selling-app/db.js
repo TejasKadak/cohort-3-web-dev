f958e5c55ea58de57d41c1abd8959de2e00e6839
tejaskadak <tejas.02001@gmail.com>
2025-03-16 11:19:58 +0530

    updated

---
+++ Week-8/backend-of-course-selling-app/db.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ObjectId = mongoose.Types.ObjectId;
+
+const userSchema = new Schema({
+    email: {type: String, unique: true},
+    password: String,
+    firstName: String,
+    lastName: String,
+});
+
+const adminSchema = new Schema({
+    email: { type: String, unique: true},
+    password: String,
+    firstName: String,
+    lastName: String,
+});
+
+const courseSchema = new Schema({
+    title: String,
+    description: String,
+    price: Number,
+    imageUrl: String,
+    creatorId: ObjectId
+});
+
+const purchaseSchema = new Schema({
+    userId: ObjectId,
+    courseId: ObjectId
+});
+
+const userModel = mongoose.Model("user", userSchema);
+const adminModel = mongoose.Model("admin", adminSchema);
+const courseModel = mongoose.Model("course", courseSchema);
+const purchasemodel = mongoose.Model("purchse", purchaseSchema)
+
+module.exports = {
+    userModel,
+    adminModel, 
+    courseModel,
+    purchasemodel
+}
---
+++ Week-8/backend-of-course-selling-app/routes/admin.js
@@ -0,0 +1,26 @@
+const { Router } = require("express");
+const adminRouter = Router();
+
+adminRouter.post("/signup", function(req, res) {
+
+})
+
+adminRouter.post("/signin", function(req, res) {
+
+})
+adminRouter.post("/course", function(req, res) {
+
+})
+
+adminRouter.put("/couse", function(req, res) {
+
+})
+
+adminRouter.get("/couse/bulk", function(req, res) {
+
+})
+
+
+module.exports = {
+    adminRouter: adminRouter
+}
