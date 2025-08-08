9c0c70f5bd4fbbd643339487c9d6633df7326a52
tejaskadak <tejas.02001@gmail.com>
2025-08-08 22:46:29 +0530

    updated

---
+++ week-14/lect-1/dist/index.js
@@ -0,0 +1,33 @@
+"use strict";
+// let x:number | string = 1
+// x = "tejas"
+// console.log(x)
+Object.defineProperty(exports, "__esModule", { value: true });
+// function greet(firstName: string){
+//     console.log("hello " + firstName)
+// }
+// greet("Tejas")
+// function sum(a:number , b:number){
+//     return a + b;
+// }
+// let ans = sum(1,2);
+// console.log(ans)
+// function isLegal(age:number){
+//     if(age >= 18){
+//         return true;
+//     } else{
+//         return false;
+//     }
+// }
+// if(isLegal(9)){
+//     console.log("user can vote");
+// } else{
+//     console.log("user cannot vote")
+// }
+function delayedCall(fn) {
+    setTimeout(fn, 1000);
+}
+delayedCall(function () {
+    console.log("hindiiii");
+});
+//# sourceMappingURL=index.js.map
